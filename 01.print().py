a45306069e6e73466bc060d8cbca89b2285ce8d1
Omar_Abdulaziz <amoor350om@hotmail.com>
2019-08-21 18:48:17 +0300

    first week_02

---
+++ 01.print().py
@@ -0,0 +1,9 @@
+print("hello world")
+
+print("hello python's world")
+
+if 5 > 2:
+    print("That's Right")
+
+if 5 > 2 :
+print("That's Right")
---
+++ 02.comments.py
@@ -0,0 +1,18 @@
+# 02. comments
+
+####
+# 01. This is a comment (one line comment)
+####
+print("first type") # (it also can be in the smae line of the code )
+
+# comments with codes
+#print("Ignored code")
+print("hello world!")
+
+
+####
+""" 02. This is a comment
+(multi lines) """
+####
+
+print("Python..!")
