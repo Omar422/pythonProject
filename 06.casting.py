9ee949135de1e30e77c6c54f68e9a60121708a09
Omar_Abdulaziz <amoor350om@hotmail.com>
2019-08-25 22:43:34 +0300

    second week_06

---
+++ 06.casting.py
@@ -0,0 +1,38 @@
+# 06. Casting
+
+
+x, y, z = 4, 3.0, "7"
+print("The main values and types are:")
+print(x)
+print(type(x))
+print(y)
+print(type(y))
+print(z)
+print(type(z))
+
+print("The values and types after using int() function are:")
+x, y, z = int(x), int(y), int(z)
+print(x)
+print(type(x))
+print(y)
+print(type(y))
+print(z)
+print(type(z))
+
+print("The values and types after using float() function are:")
+x, y, z = float(x), float(y), float(z)
+print(x)
+print(type(x))
+print(y)
+print(type(y))
+print(z)
+print(type(z))
+
+print("The values and types after using str() function are:")
+x, y, z = str(x), str(y), str(z)
+print(x)
+print(type(x))
+print(y)
+print(type(y))
+print(z)
+print(type(z))
