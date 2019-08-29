c88c93eed29d19ef0827cf9c3378c42282ba0e6f
Omar_Abdulaziz <amoor350om@hotmail.com>
2019-08-29 22:36:18 +0300

    second week_10

---
+++ 10.operator.py
@@ -0,0 +1,16 @@
+#10. Python Operators
+
+a = """ 10_th Day: Operators (7 groups)
+    1- Arithmetic: ( + , - , * ,  / ,  % ,  ** ,  // )
+    2- Assignment: ( = , += , -= , *= , /= , %= , //=  , **= , &= , |= , ^= , >>= , <<= )
+        ** Assignment operators are used to assign values to variables.
+    3- Comparison: ( == , != , > , < , >= , <= )
+"""
+t1 = 5*(9-1/2)
+t1 = 10 % 3
+t2 = 10
+t2 *= 2
+t3 = 10 != 5
+print(t1)
+print(t2)
+print(t3)
