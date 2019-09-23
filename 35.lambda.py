8e507e923cf44a9174594ba51dea31f09c8e8c09
Omar_Abdulaziz <amoor350om@hotmail.com>
2019-09-24 01:56:11 +0300

    sixth week_35

--- 35.lambda.py
+++ 35.lambda.py
@@ -7,7 +7,10 @@
 """
 # we need to assign lambda to a variable so we can call it using variable name
 x = lambda x: x + 10
-print("lambda add the argument to 10.. x(5)\n"+str(x(5)))
+print("lambda adds the argument to 10.. x(5)\n"+str(x(5)))
 
 x = lambda x,y : x * y
 print("\ncall lambda function which was stored in variable x, it multiplies the arguments.. x(5,6)\n"+str(x(5,6)))
+
+x = lambda x,y,z : x + y + z
+print("\nlambda sums the arguments .. x(5,5.2,10.8)\n"+str(x(5,5.2,10.8)))
