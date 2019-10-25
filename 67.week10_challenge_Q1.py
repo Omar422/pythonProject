91cabb9b2ab26ef9985ad7bda423adae333e0487
Omar_Abdulaziz <amoor350om@hotmail.com>
2019-10-25 23:38:24 +0300

    tenth week_67

---
+++ 67.week10_challenge_Q1.py
@@ -0,0 +1,12 @@
+# 67 . Week Challenge Q1
+
+"""
+# take the first and last letter from the user name then print them
+"""
+
+print("What is the first letter of your name? ")
+f = input()
+print("What is the last letter of your name? ")
+l = input()
+
+print("Your name start with: \""+f+"\", & end with: \""+l+"\"")
