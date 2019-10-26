b95fa64db63318a43bc0d57c91a1543ec49e0634
Omar_Abdulaziz <amoor350om@hotmail.com>
2019-10-26 18:06:18 +0300

    tenth week_68

---
+++ 68.week10_challenge_Q2.py
@@ -0,0 +1,5 @@
+# 68 . Week Challenge Q2
+# use format() method to print "Dear Ahmad Ali, Your current balance is 53.44 $"
+
+txt = "Dear {fname} {lname}, Your current balance is {balance} $"
+print(txt.format(fname="Ahmad", lname="Ali", balance=53.44))
